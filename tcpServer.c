7b709ee1bf30d59b6f3405dcfb6041bfcf2421c7
Jose Luis Rodriguez <joserh1206@gmail.com>
2019-05-19 22:41:54 -0600

    getPlayersToGame again

--- tcpServer.c
+++ tcpServer.c
@@ -151,6 +151,29 @@ void beginGame(char *username){
 	getPlayersToGame(username);
 }
 
+void getPlayersToGame(char *username)
+{
+  sqlite3 *db = openDatabase();
+  sqlite3_stmt *stmt;
+  char sql[1024];
+  int rc;
+	sprintf(sql,"select * from Users where Users.id_user not in (select distinct Users.id_user from Users inner join Game on Users.id_user = Game.id_user where Game.id_game in (select Game.id_game from Game inner join Users on Game.id_user = Users.id_user and Users.username = '%s'))", username);
+	rc = sqlite3_prepare_v2(db, sql, -1, &stmt, NULL);
+	sqlite3_bind_int(stmt,1,16);
+	if (rc != SQLITE_OK)
+  {
+    fprintf(stderr, "Failed get players: %s\n", sqlite3_errmsg(db));
+    sqlite3_close(db);
+  }
+  else
+  {
+		while((rc=sqlite3_step(stmt)) == SQLITE_ROW){
+			printf("%s and %s\n", sqlite3_column_text(stmt,0), sqlite3_column_text(stmt,1));
+		}
+  }
+  closeDatabase(db);
+}
+
 int checkUsernamePassword(char *username, char *password)
 {
   sqlite3 *db = openDatabase();
