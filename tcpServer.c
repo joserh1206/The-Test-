3e6f9b440c986c653726ccd346f19b8988147e51
gerardo0807 <gerardogutierrezquiros.gg@gmail.com>
2019-05-27 00:16:42 -0600

    Update (funcion obtener ultimas dos preguntas)

--- tcpServer.c
+++ tcpServer.c
@@ -14,6 +14,7 @@ sqlite3* openDatabase();
 int checkUsernamePassword(char *username, char *password);
 int insertPlayerIntoDB(char *username, char *password);
 void beginGame(char *username);
+char* getTwoQuestionsLastPLayer(int game);
 char* getAllGamesAndStatisticsForPlayer(int player);
 char* getRightAndWrongPlayer(int player);
 int getCorrectAnswer(int game, int question);
@@ -354,6 +355,34 @@ int main(){
 	}
 	close(newSocket);
 	return 0;
+}
+
+//Obtiene las ultimas dos preguntas respondidas
+char* getTwoQuestionsLastPLayer(int game){
+	sqlite3 *db = openDatabase();
+  char sql[1024], out[2048];
+	char *buffer;
+  int rc;
+	sprintf(sql, "select QuestionsPerGame.id_question from QuestionsPerGame where QuestionsPerGame.id_game = %d limit 2 offset (select count(*) from QuestionsPerGame where QuestionsPerGame.id_game = %d)-2;", game, game);
+	rc = sqlite3_prepare_v2(db, sql, -1, &stmt, NULL);
+	sqlite3_bind_int(stmt,1,16);
+	if (rc != SQLITE_OK)
+  {
+    fprintf(stderr, "Failed get questions: %s\n", sqlite3_errmsg(db));
+    closeDatabase(db);
+  }
+  else
+  {
+		bzero(&out, sizeof(out));
+		bzero(&buffer, sizeof(buffer));
+		while((rc=sqlite3_step(stmt)) == SQLITE_ROW){
+			strcat(out, sqlite3_column_text(stmt,0));
+			strcat(out, sqlite3_column_text(stmt,1));
+		}
+  }
+	buffer = out;
+	closeDatabase(db);
+	return buffer;
 }
 
 //Obtener respuesta correct, recibe el id del juego y id de la pregunta
